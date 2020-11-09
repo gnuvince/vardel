7ea045f449e791d025a11d328c7084ed377f7bcf
Vincent Foley <vfoley@gmail.com>
2020-11-09 16:09:07 -0500

    Add slice encoders

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,32 @@ macro_rules! make_encoder {
             }
             return None;
         }
+    };
+}
 
+macro_rules! make_slice_encoder {
+    ($func_name:ident, $single_encoder:ident, $ty:ty) => {
+        pub fn $func_name(xs: &[$ty]) -> Option<Vec<u8>> {
+            let mut out: Vec<u8> = Vec::new();
+            let mut buf: [u8; 16] = [0; 16];
+
+            if xs.is_empty() {
+                return Some(out);
+            }
+
+            let mut prev = xs[0];
+            let n = $single_encoder(prev, &mut buf)?;
+            out.extend_from_slice(&buf[..n]);
+
+            for x in &xs[1..] {
+                let delta = *x - prev;
+                let n = $single_encoder(delta, &mut buf)?;
+                out.extend_from_slice(&buf[..n]);
+                prev = *x;
+            }
+
+            return Some(out);
+        }
     };
 }
 
@@ -21,6 +46,11 @@ make_encoder!(encode_u8, u8);
 make_encoder!(encode_u16, u16);
 make_encoder!(encode_u32, u32);
 make_encoder!(encode_u64, u64);
+
+make_slice_encoder!(encode_u8_slice, encode_u8, u8);
+make_slice_encoder!(encode_u16_slice, encode_u16, u16);
+make_slice_encoder!(encode_u32_slice, encode_u32, u32);
+make_slice_encoder!(encode_u64_slice, encode_u64, u64);
 
 #[test]
 fn test_encode_u8() {
@@ -69,4 +99,27 @@ fn test_encode_u32() {
 
     assert_eq!(Some(2), encode_u32(300, &mut out[..]));
     assert_eq!(&[0xac, 0x02], &out[0..2]);
+}
+
+
+#[test]
+fn test_encode_u8_slice() {
+    let r = encode_u8_slice(&[]);
+    assert!(r.is_some());
+    assert!(r.unwrap().is_empty());
+
+    let r = encode_u8_slice(&[1, 2, 3]);
+    assert!(r.is_some());
+    assert_eq!(vec![1, 1, 1], r.unwrap());
+
+    let r = encode_u8_slice(&[5, 10, 160]);
+    assert!(r.is_some());
+    assert_eq!(vec![5, 5, 0x96, 0x01], r.unwrap());
+}
+
+#[test]
+fn test_encode_u32_slice() {
+    let r = encode_u32_slice(&[150, 300]);
+    assert!(r.is_some());
+    assert_eq!(vec![0x96, 0x01, 0x96, 0x01], r.unwrap());
 }
